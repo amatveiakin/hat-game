e58c2a9d269d68ece4f81cdd6499ff5173495ed3
Andrei Matveiakin <a.matveyakin@gmail.com>
2020-04-17 02:01:59 +0200

    Update theme.

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xff4b0082),
+        accentColor: Color(0xffccad00),
+      ),
       title: 'Hat Game',
       home: StartScreen(),
     );
