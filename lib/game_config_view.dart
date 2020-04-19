982f5fcae7e0aaeb12f659212f19b3e5999c6e7b
Andrei Matveiakin <a.matveyakin@gmail.com>
2020-04-19 19:18:11 +0200

    Allow to add and delete players.

--- lib/game_config_view.dart
+++ lib/game_config_view.dart
@@ -1,26 +1,62 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hatgame/game_view.dart';
+import 'package:hatgame/theme.dart';
 
 class PlayersView extends StatefulWidget {
   @override
   createState() => _PlayersViewState();
 }
 
+class _PlayerData {
+  String name;
+  TextEditingController controller = TextEditingController();
+  FocusNode focusNode = FocusNode();
+
+  _PlayerData(this.name);
+
+  void dispose() {
+    controller.dispose();
+    focusNode.dispose();
+  }
+}
+
 class _PlayersViewState extends State<PlayersView> {
-  var players = <String>[
-    'Vasya',
-    'Petya',
-    'Masha',
-    'Dasha',
-  ];
-  List<Widget> items = [];
+  var _players = <_PlayerData>[];
+  List<Widget> _items = [];
+
+  void _addPlayer(String name) {
+    setState(() {
+      final playerData = _PlayerData(name);
+      playerData.controller.text = (name);
+      playerData.controller.addListener(() {
+        // Don't call setState, because TextField updates itself.
+        playerData.name = playerData.controller.text;
+      });
+      playerData.focusNode.requestFocus();
+      _players.add(playerData);
+    });
+  }
+
+  void _deletePlayer(_PlayerData player) {
+    player.dispose();
+    setState(() {
+      _players.remove(player);
+    });
+  }
 
   List<Widget> _addDividers(List<ListTile> items) {
     var result = <Widget>[];
     int i = 0;
     for (final item in items) {
       if (i > 0 && i % 2 == 0) {
-        result.add(Divider(key: UniqueKey()));
+        result.add(Divider(
+          key: UniqueKey(),
+          color: MyColors.accent,
+          thickness: 3.0,
+          height: 20.0,
+        ));
       }
       result.add(item);
       i++;
@@ -31,22 +67,65 @@ class _PlayersViewState extends State<PlayersView> {
   int _getPlayerIndex(int itemIndex) {
     int playerIndex = 0;
     for (int i = 0; i < itemIndex; i++) {
-      if (!(items[i] is Divider)) playerIndex++;
+      if (!(_items[i] is Divider)) playerIndex++;
     }
-    return playerIndex;
+    return min(playerIndex, _players.length - 1);
   }
 
   void _makeItems() {
-    items = _addDividers(players
-        .map((name) => ListTile(
-              key: UniqueKey(),
-              title: Text(name),
-            ))
-        .toList());
+    final listItemPadding = EdgeInsets.fromLTRB(10, 1, 10, 1);
+    final listItemPaddingSmallRight = EdgeInsets.fromLTRB(listItemPadding.left,
+        listItemPadding.top, listItemPadding.right / 2, listItemPadding.bottom);
+    var _playerItems = <ListTile>[];
+    for (int i = 0; i < _players.length; ++i) {
+      final player = _players[i];
+      _playerItems.add(ListTile(
+        key: UniqueKey(),
+        contentPadding: listItemPaddingSmallRight,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration(filled: true, border: InputBorder.none),
+                focusNode: player.focusNode,
+                controller: player.controller,
+              ),
+            ),
+            SizedBox(width: 12),
+            // TODO: Make sure we don't have two drag handles on iOS.
+            Icon(Icons.drag_handle),
+            SizedBox(width: 4),
+            IconButton(
+              padding: EdgeInsets.zero,
+              icon: Icon(Icons.clear),
+              onPressed: () => _deletePlayer(player),
+            ),
+          ],
+        ),
+      ));
+    }
+    _playerItems.add(ListTile(
+        key: UniqueKey(),
+        contentPadding: listItemPadding,
+        title: OutlineButton(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          onPressed: () => setState(() {
+            _addPlayer('');
+          }),
+          child: Text('Add player'),
+        )));
+    _items = _addDividers(_playerItems);
   }
 
   @override
   void initState() {
+    // TODO: Delete before prod release.
+    _addPlayer('Vasya');
+    _addPlayer('Petya');
+    _addPlayer('Masha');
+    _addPlayer('Dasha');
     super.initState();
     _makeItems();
   }
@@ -55,6 +134,14 @@ class _PlayersViewState extends State<PlayersView> {
   void setState(fn) {
     super.setState(fn);
     _makeItems();
+  }
+
+  @override
+  void dispose() {
+    for (final player in _players) {
+      player.dispose();
+    }
+    super.dispose();
   }
 
   @override
@@ -68,12 +155,12 @@ class _PlayersViewState extends State<PlayersView> {
           if (newIndex > oldIndex) {
             newIndex--;
           }
-          final p = players.removeAt(oldIndex);
-          players.insert(newIndex, p);
+          final p = _players.removeAt(oldIndex);
+          _players.insert(newIndex, p);
         });
       },
       scrollDirection: Axis.vertical,
-      children: items,
+      children: _items,
     );
   }
 }
