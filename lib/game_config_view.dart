1d102e7fdf53fb8a83d84d3e22c0aad6490943dc
Andrei Matveiakin <a.matveyakin@gmail.com>
2020-04-19 02:23:22 +0200

    Players dialog stub.

--- lib/game_config_view.dart
+++ lib/game_config_view.dart
@@ -1,6 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:hatgame/game_view.dart';
 
+class PlayersView extends StatefulWidget {
+  @override
+  createState() => _PlayersViewState();
+}
+
+class _PlayersViewState extends State<PlayersView> {
+  var players = <String>[
+    'Vasya',
+    'Petya',
+    'Masha',
+    'Dasha',
+  ];
+  List<Widget> items = [];
+
+  List<Widget> _addDividers(List<ListTile> items) {
+    var result = <Widget>[];
+    int i = 0;
+    for (final item in items) {
+      if (i > 0 && i % 2 == 0) {
+        result.add(Divider(key: UniqueKey()));
+      }
+      result.add(item);
+      i++;
+    }
+    return result;
+  }
+
+  int _getPlayerIndex(int itemIndex) {
+    int playerIndex = 0;
+    for (int i = 0; i < itemIndex; i++) {
+      if (!(items[i] is Divider)) playerIndex++;
+    }
+    return playerIndex;
+  }
+
+  void _makeItems() {
+    items = _addDividers(players
+        .map((name) => ListTile(
+              key: UniqueKey(),
+              title: Text(name),
+            ))
+        .toList());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _makeItems();
+  }
+
+  @override
+  void setState(fn) {
+    super.setState(fn);
+    _makeItems();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ReorderableListView(
+      onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          // TODO: Swap teams instead (?)
+          oldIndex = _getPlayerIndex(oldIndex);
+          newIndex = _getPlayerIndex(newIndex);
+          if (newIndex > oldIndex) {
+            newIndex--;
+          }
+          final p = players.removeAt(oldIndex);
+          players.insert(newIndex, p);
+        });
+      },
+      scrollDirection: Axis.vertical,
+      children: items,
+    );
+  }
+}
+
 class GameConfigView extends StatefulWidget {
   @override
   createState() => _GameConfigViewState();
@@ -33,7 +110,7 @@ class _GameConfigViewState extends State<GameConfigView> {
         ),
         body: TabBarView(
           children: [
-            Center(child: Text('players')),
+            Center(child: PlayersView()),
             Center(child: Text('settings')),
           ],
         ),
