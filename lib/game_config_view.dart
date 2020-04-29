dd2018d9a68b0dd90ddaf6607077a39daa6635f7
Andrei Matveiakin <a.matveyakin@gmail.com>
2020-04-29 20:46:37 +0200

    Add wide layout for settings.

--- lib/game_config_view.dart
+++ lib/game_config_view.dart
@@ -46,7 +46,7 @@ class _GameConfigViewState extends State<GameConfigView>
         icon: Icon(Icons.people),
       ),
       Tab(
-        text: 'Players ($numPlayers)',
+        text: 'Players: $numPlayers',
         // TODO: Replace squares with person icons.
         icon: Icon(OMIcons.ballot),
       ),
@@ -135,83 +135,153 @@ class _GameConfigViewState extends State<GameConfigView>
         }
 
         final tabs = _buildTabs(gameConfig.players.names.length);
-        return Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: localGameData.onlineMode
-              ? AppBar(
-                  automaticallyImplyLeading: false,
-                  title: Text('Game ID: ${localGameData.gameID}'),
-                  // For some reason PreferredSize affects not only the bottom of
-                  // the AppBar but also the title, making it misaligned with the
-                  // normal title text position. Hopefully this is not too
-                  // noticeable. Without PreferredSize the AppBar is just too fat.
-                  bottom: PreferredSize(
-                    preferredSize: Size.fromHeight(64.0),
-                    child: TabBar(
-                      controller: _tabController,
-                      tabs: tabs,
-                    ),
-                  ),
+        final configViews = [
+          RulesConfigView(
+            config: gameConfig.rules,
+            configController: configController,
+          ),
+          TeamingConfigView(
+            onlineMode: localGameData.onlineMode,
+            config: gameConfig.teaming,
+            configController: configController,
+          ),
+          localGameData.onlineMode
+              ? OnlinePlayersConfigView(
+                  playersConfig: gameConfig.players,
                 )
-              : PreferredSize(
-                  preferredSize: Size.fromHeight(64.0),
-                  child: AppBar(
-                    automaticallyImplyLeading: false,
-                    flexibleSpace: SafeArea(
-                      child: TabBar(
-                        controller: _tabController,
-                        tabs: tabs,
-                      ),
-                    ),
-                  ),
+              : throw UnimplementedError('Offline mode not supported yet'),
+          /*
+                OfflinePlayersConfigView(
+                  teamingConfig: gameConfig.teaming,
+                  initialPlayersConfig: gameConfig.players,
+                  configController: configController,
                 ),
-          body: Column(
-            children: [
-              Expanded(
-                child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    RulesConfigView(
-                      config: gameConfig.rules,
-                      configController: configController,
-                    ),
-                    TeamingConfigView(
-                      onlineMode: localGameData.onlineMode,
-                      config: gameConfig.teaming,
-                      configController: configController,
-                    ),
-                    localGameData.onlineMode
-                        ? OnlinePlayersConfigView(
-                            playersConfig: gameConfig.players,
-                          )
-                        : throw UnimplementedError(
-                            'Offline mode not supported yet'),
-                    /*
-                          OfflinePlayersConfigView(
-                            teamingConfig: gameConfig.teaming,
-                            initialPlayersConfig: gameConfig.players,
-                            configController: configController,
-                          ),
-                    */
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 12.0),
-                child: WideButton(
-                  onPressed: isAdmin ? () => _startGame(gameConfig) : null,
-                  onPressedDisabled: () {
-                    final snackBar = SnackBar(
-                        content: Text('Only the host can start the game.'));
-                    Scaffold.of(context).showSnackBar(snackBar);
-                  },
-                  color: MyTheme.accent,
-                  child: Text('Start Game'),
-                ),
-              ),
-            ],
+          */
+        ];
+        final startButton = Padding(
+          padding: EdgeInsets.symmetric(vertical: 12.0),
+          child: WideButton(
+            onPressed: isAdmin ? () => _startGame(gameConfig) : null,
+            onPressedDisabled: () {
+              final snackBar =
+                  SnackBar(content: Text('Only the host can start the game.'));
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+            color: MyTheme.accent,
+            child: Text('Start Game'),
           ),
         );
+
+        const double configBoxWidth = 480;
+        const double configBoxMargin = 16;
+        final double wideLayoutWidth =
+            (configBoxWidth + 2 * configBoxMargin) * numTabs;
+        final bool wideLayout =
+            MediaQuery.of(context).size.width >= wideLayoutWidth;
+
+        if (!wideLayout) {
+          // One-column view for phones and tablets in portrait mode.
+          final tabBar = TabBar(
+            controller: _tabController,
+            tabs: tabs,
+          );
+          return Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: localGameData.onlineMode
+                ? AppBar(
+                    automaticallyImplyLeading: false,
+                    title: Text('Hat Game ID: ${localGameData.gameID}'),
+                    // For some reason PreferredSize affects not only the bottom of
+                    // the AppBar but also the title, making it misaligned with the
+                    // normal title text position. Hopefully this is not too
+                    // noticeable. Without PreferredSize the AppBar is just too fat.
+                    bottom: PreferredSize(
+                        preferredSize: Size.fromHeight(64.0), child: tabBar),
+                  )
+                : PreferredSize(
+                    preferredSize: Size.fromHeight(64.0),
+                    child: AppBar(
+                      automaticallyImplyLeading: false,
+                      flexibleSpace: SafeArea(child: tabBar),
+                    ),
+                  ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: configViews,
+                  ),
+                ),
+                startButton,
+              ],
+            ),
+          );
+        } else {
+          final configBoxes = List<Widget>();
+          for (int i = 0; i < numTabs; i++) {
+            configBoxes.add(
+              SizedBox(
+                width: configBoxWidth,
+                child: Padding(
+                  padding: EdgeInsets.all(configBoxMargin),
+                  child: Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          color: MyTheme.primary,
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            tabs[i].text,
+                            // Use the same text style as AppBar.
+                            // TODO: Why is font bigger than in actual AppBar?
+                            style:
+                                Theme.of(context).textTheme.headline6.copyWith(
+                                      // TODO: Take color from the theme.
+                                      color: Colors.white,
+                                    ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(8),
+                            child: configViews[i],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+          // Multi-column view for tablets in landscape mode and desktops.
+          return Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              title: Text(localGameData.onlineMode
+                  ? 'Hat Game ID: ${localGameData.gameID}'
+                  : 'Hat Game'),
+            ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: configBoxes,
+                  ),
+                ),
+                SizedBox(
+                  width: wideLayoutWidth,
+                  child: startButton,
+                )
+              ],
+            ),
+          );
+        }
       },
     );
   }
