deb84f2f23b6b480d14b3b114eb3b20061282286
Andrei Matveiakin <a.matveyakin@gmail.com>
2022-02-04 23:02:46 +0300

    Fix `core/duplicate-app` on mobile
    The error was:
        flutter FirebaseException ([core/duplicate-app] A Firebase App
        named "[DEFAULT]" already exists)

--- hatgame/lib/main.dart
+++ hatgame/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:easy_localization_loader/easy_localization_loader.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hatgame/about_screen.dart';
 import 'package:hatgame/app_settings.dart';
@@ -16,8 +17,8 @@ import 'package:hatgame/util/ntp_time.dart';
 import 'package:hatgame/util/sounds.dart';
 
 Future<void> _initFirestore() async {
-  await Firebase.initializeApp(
-      options: FirebaseOptions(
+  final option = kIsWeb
+      ? FirebaseOptions(
           apiKey: "AIzaSyBX7jo-zBfl0EKR6s7-ph4NhifkhIPzvzw",
           authDomain: "hatgame.firebaseapp.com",
           databaseURL: "https://hatgame.firebaseio.com",
@@ -25,7 +26,9 @@ Future<void> _initFirestore() async {
           storageBucket: "hatgame.appspot.com",
           messagingSenderId: "761993928349",
           appId: "1:761993928349:web:a9f63af19ae7b1c9b87e74",
-          measurementId: "G-TNXGM73WRF"));
+          measurementId: "G-TNXGM73WRF")
+      : null;
+  await Firebase.initializeApp(options: option);
 
   // Enable offline mode. This is the default for Android and iOS, but
   // on web it need to be enabled explicitly:
