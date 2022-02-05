f2bf23ee05833e18fa5c4d02881afac3cdbedb3f
Andrei Matveiakin <a.matveyakin@gmail.com>
2022-02-05 21:50:20 +0300

    Change color scheme to brown/yellow-green

--- hatgame/lib/theme.dart
+++ hatgame/lib/theme.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
-// TODO: Change color scheme (how about brown/green, paper-ish/leaf-ish?)
 class MyTheme {
-  static const primary = MaterialColor(0xff4b0082, {
-    50: Color(0xffdaa8ff),
-    100: Color(0xffdaa8ff),
-    200: Color(0xffbf68ff),
-    300: Color(0xffbf68ff),
-    400: Color(0xff882acc),
-    500: Color(0xff4b0082),
-    600: Color(0xff4b0082),
-    700: Color(0xff310062),
-    800: Color(0xff310062),
-    900: Color(0xff210041),
+  static const primary = MaterialColor(0xff5d4037, {
+    50: Color(0xffefebe9),
+    100: Color(0xffd7ccc8),
+    200: Color(0xffbcaaa4),
+    300: Color(0xffa1887f),
+    400: Color(0xff8d6e63),
+    500: Color(0xff795548),
+    600: Color(0xff6d4c41),
+    700: Color(0xff5d4037),
+    800: Color(0xff4e342e),
+    900: Color(0xff3e2723),
   });
-  static const primaryDark = Color(0xff310062);
-  static const secondary = Color(0xffccad00);
-  static const secondaryDark = Color(0xffb59900);
+  static const primaryDark = Color(0xff4e342e);
+  static const secondary = Color(0xffb6bd4c);
+  static const secondaryDark = Color(0xff9ea53b);
 }
