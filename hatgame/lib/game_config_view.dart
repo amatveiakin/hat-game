amatveiakin/hat-game

commit 157224698860cd702047ce24ce5231961bebba81

Don't show error when pressing Next on rules page

hatgame/lib/game_config_view.dart
@@ -120,6 +120,11 @@ class _GameConfigViewState extends State<GameConfigView>
   }
 
   void _next(GameConfig gameConfig) async {
+    if (_tabController.index < numTabs - 1) {
+      _tabController.animateTo(_tabController.index + 1);
+      return;
+    }
+
     try {
       GameController.preGameCheck(gameConfig);
     } on InvalidOperation catch (e) {

commit 4003b6cc4448f41464442d5da9756b1337c16b15

Fix: needed to click Next twice on wide layouts
Broken in 157224698860cd702047ce24ce5231961bebba81

hatgame/lib/game_config_view.dart
@@ -120,8 +120,8 @@ class _GameConfigViewState extends State<GameConfigView>
     );
   }
 
-  void _next(GameConfig gameConfig) async {
-    if (_tabController.index < numTabs - 1) {
+  void _next(GameConfig gameConfig, bool wideLayout) async {
+    if (!wideLayout && _tabController.index < numTabs - 1) {
       _tabController.animateTo(_tabController.index + 1);
       return;
     }
@@ -211,9 +211,10 @@ class _GameConfigViewState extends State<GameConfigView>
         ]),
       ),
     ];
+    final wideLayout = SectionsScaffold.useWideLayout(context, sections.length);
     Assert.eq(sections.length, numTabs);
     final startButton = WideButton(
-      onPressed: isAdmin ? () => _next(gameConfig) : null,
+      onPressed: isAdmin ? () => _next(gameConfig, wideLayout) : null,
       onPressedDisabled: isAdmin
           ? null
           : () => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
@@ -233,6 +234,7 @@ class _GameConfigViewState extends State<GameConfigView>
           ? context.tr('hat_game_id', args: [localGameData.gameID.toString()])
           : context.tr('hat_game'),
       appTitlePresentInNarrowMode: localGameData.onlineMode,
+      wideLayout: wideLayout,
       actions: localGameData.onlineMode
           ? [
               IconButton(

hatgame/lib/widget/sections_scaffold.dart
@@ -3,6 +3,15 @@ import 'package:hatgame/theme.dart';
 import 'package:hatgame/util/assertion.dart';
 import 'package:hatgame/widget/constrained_scaffold.dart';
 
+const double _minBoxWidth = 360;
+const double _maxBoxWidth = 480;
+const double _boxMargin = 16;
+
+double _minWideLayoutWidth(int numSections) =>
+    (_minBoxWidth + 2 * _boxMargin) * numSections;
+double _maxWideLayoutWidth(int numSections) =>
+    (_maxBoxWidth + 2 * _boxMargin) * numSections;
+
 class SectionTitleData {
   final Widget icon;
 
@@ -21,10 +30,14 @@ class SectionsScaffold extends StatelessWidget {
   final bool appBarAutomaticallyImplyLeading;
   final String appTitle;
   final bool appTitlePresentInNarrowMode;
+  final bool wideLayout;
   final List<SectionData> sections;
   final List<Widget>? actions;
   final TabController? tabController;
   final Widget? bottomWidget;
+
+  static bool useWideLayout(BuildContext context, int numSections) =>
+      MediaQuery.of(context).size.width >= _minWideLayoutWidth(numSections);
 
   SectionsScaffold({
     super.key,
@@ -32,6 +45,7 @@ class SectionsScaffold extends StatelessWidget {
     required this.appBarAutomaticallyImplyLeading,
     required this.appTitle,
     required this.appTitlePresentInNarrowMode,
+    required this.wideLayout,
     required this.sections,
     this.actions,
     this.tabController,
@@ -47,16 +61,6 @@ class SectionsScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const double minBoxWidth = 360;
-    const double maxBoxWidth = 480;
-    const double boxMargin = 16;
-    final double minWideLayoutWidth =
-        (minBoxWidth + 2 * boxMargin) * sections.length;
-    final double maxWideLayoutWidth =
-        (maxBoxWidth + 2 * boxMargin) * sections.length;
-    final bool wideLayout =
-        MediaQuery.of(context).size.width >= minWideLayoutWidth;
-
     if (!wideLayout) {
       // One-column view for phones and tablets in portrait mode.
       // TODO: Automatically increase padding on large screens.
@@ -118,7 +122,7 @@ class SectionsScaffold extends StatelessWidget {
         boxes.add(
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(boxMargin),
+              padding: const EdgeInsets.all(_boxMargin),
               child: Card(
                 clipBehavior: Clip.antiAlias,
                 child: Column(
@@ -158,7 +162,7 @@ class SectionsScaffold extends StatelessWidget {
             children: [
               Expanded(
                 child: SizedBox(
-                  width: maxWideLayoutWidth,
+                  width: _maxWideLayoutWidth(sections.length),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: boxes,